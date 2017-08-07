4734ac901e41ce58bad5d8acdcff9e31ce391fe5
Damian Reiter <damian.reiter@gmail.com>
2017-08-07 15:12:48 -0400

    organization

--- Unit 15 The Colour Game/colourGame.js
+++ Unit 15 The Colour Game/colourGame.js
@@ -3,52 +3,15 @@ var rgbHeader = document.querySelector("#colourDisplay");
 var header = document.querySelector("h1");
 var message = document.querySelector("#message");
 var resetBtn = document.querySelector("#reset");
+var winnerBox;
+
+resetGame();
 
 resetBtn.addEventListener("click", function(){
     header.style.background = "#232323"
     winnerBox.classList.remove("winner");
-    colours = generateRandomColours();
-    winner = Math.floor((Math.random() * 6));
-    winnerBox = boxes[winner];
-    winnerBox.classList.add("winner");
-    for(var i = 0; i < boxes.length; i++){
-        boxes[i].style.backgroundColor = colours[i];
-        boxes[i].addEventListener("click", function(){
-            if(this.classList.contains("winner")){
-                message.textContent = "Correct";
-                changeColours();
-            } else {
-                this.style.backgroundColor = "#232323";
-                message.textContent= "Try Again";
-            }
-        });
-    }
-    rgbHeader.textContent = winnerBox.style.backgroundColor;
+    resetGame();
 })
-
-var winner = Math.floor((Math.random() * 6));
-var winnerBox = boxes[winner];
-console.log(winner);
-
-var colours = generateRandomColours();
-
-for(var i = 0; i < boxes.length; i++){
-    console.log("Box " + (i+1) + ": " + colours[i]);
-    boxes[i].style.backgroundColor = colours[i];
-
-    boxes[i].addEventListener("click", function(){
-        if(this.classList.contains("winner")){
-            message.textContent = "Correct";
-            changeColours();
-        } else {
-            this.style.backgroundColor = "#232323";
-            message.textContent= "Try Again";
-        }
-    });
-}
-
-winnerBox.classList.add("winner");
-rgbHeader.textContent = winnerBox.style.backgroundColor;
 
 function changeColours(colour){
     for(var i = 0; i < boxes.length; i++){
@@ -63,4 +26,31 @@ function generateRandomColours(){
         array.push("rgb(" + (Math.floor((Math.random() * 256))) + ", " + (Math.floor((Math.random() * 256))) + ", " + (Math.floor((Math.random() * 256))) + ")");
     }
     return array;
+}
+
+function resetGame(){
+    var winner = Math.floor((Math.random() * 6));
+    resetBtn.textContent = "New Colours";
+    message.textContent = "-";
+    winnerBox = boxes[winner];
+    console.log(winner);
+    var colours = generateRandomColours();
+
+    for(var i = 0; i < boxes.length; i++){
+        console.log("Box " + (i+1) + ": " + colours[i]);
+        boxes[i].style.backgroundColor = colours[i];
+
+        boxes[i].addEventListener("click", function(){
+            if(this.classList.contains("winner")){
+                message.textContent = "Correct";
+                resetBtn.textContent = "Play Again?";
+                changeColours();
+            } else {
+                this.style.backgroundColor = "#232323";
+                message.textContent= "Try Again";
+            }
+        });
+    }
+    winnerBox.classList.add("winner");
+    rgbHeader.textContent = winnerBox.style.backgroundColor;
 }
