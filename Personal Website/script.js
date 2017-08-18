79346f83a20d1b9937f9712a814ff5ad3007a5c5
DESKTOP-526JKC2\damia <damian.reiter@gmail.com>
2017-08-18 13:42:11 -0400

    more tweaking to the navbar and also cleaned up some code in the script

--- Personal Website/script.js
+++ Personal Website/script.js
@@ -20,13 +20,7 @@ jWindow.resize(function(){
     didResize = true;
 });
 
-$('#bs-navbar-collapse').on('show.bs.collapse', function() {
-    navbar.css("opacity", "1");
-    navbar.css("background", "#68EECF");
-});
-
 $('#bs-navbar-collapse').on('hide.bs.collapse', function() {
-    // navbar.css("background", "none");
     didScroll = true;
 });
 
@@ -36,7 +30,6 @@ $('#nav-about-button').on('click', function(){
     window.scroll(0, dest);
 });
 
-
 // Make the 'Projects' button on the navbar scroll you to the projects div
 $('#nav-projects-button').on('click', function(){
     var dest = $('#projects-container').offset().top - 75;
@@ -45,14 +38,6 @@ $('#nav-projects-button').on('click', function(){
 
 $('.navbar-toggle').on('click', function(){
     navbar.css("opacity", "1");
-});
-
-$('.navbar-right').on('show.bs.collapse', function(){
-    console.log("shown");
-});
-
-$('.navbar-right').on('hide.bs.collapse', function(){
-    console.log("hidden");
 });
 
 $('.dropdown').on('show.bs.dropdown', function(e){
@@ -71,17 +56,16 @@ setInterval(function(){
     // Check when window has been switched to support smaller screen sizes (such as phones)
     if(didResize){
         if(!smallScreen){
+            //switched to small
             if(jWindow.width() < 768){
                 switchedScreenSize();
-                if($('.navbar-collapse').attr("aria-expanded") == "false")
-                    navbar.css("background", "none");
+                navbar.addClass("navbar-close");
             }
         } else {
             if(jWindow.width() > 768){
                 switchedScreenSize();
             }
         }
-
         didResize = false;
     }
 
@@ -89,11 +73,9 @@ setInterval(function(){
         if(didScroll){
             var offset = $('h1').offset().top;
             if($(window).scrollTop() >= offset - 70){
-                navbar.css("background-color", "#68EECF");
-                navbar.css("box-shadow", "2px 0px 4px black")
+                navbar.addClass("navbar-close");
             } else {
-                navbar.css("background-color", "transparent");
-                navbar.css("box-shadow", "none")
+                navbar.removeClass("navbar-close");
             }
 
             didScroll = false;
@@ -101,7 +83,6 @@ setInterval(function(){
 }, 250);
 
 function switchedScreenSize(){
-    navbar.css("background", "#68EECF")
     didScroll = true;
     smallScreen = !smallScreen;
 }
